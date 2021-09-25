dd1f3615ebea34309997d9bb37f33b8674db2ea8
Christian Westrom <c.westrom@westrom.xyz>
2021-09-25 01:09:11 +0000

    Change gui to a dialog + various refactoring

--- src/wallpaper_tool/core.clj
+++ src/wallpaper_tool/core.clj
@@ -1,24 +1,19 @@
 (ns wallpaper-tool.core
   (:require [clojure.java.io :as io]
             [seesaw.bind :as b]
-            [seesaw.color :as c]
             [seesaw.core :as s]
+            [seesaw.dev :as dev]
             [seesaw.graphics :as g])
-  (:import java.awt.Dimension
-           java.awt.image.BufferedImage
-           javax.imageio.ImageIO))
+  (:import (java.awt Canvas Graphics2D Dimension)
+           (java.awt.image BufferedImage)
+           (javax.imageio ImageIO)))
 
-;; Prod env
-(def env
-  (atom {:on-close :exit}))
+(set! *warn-on-reflection* true)
 
-;; Dev env
-#_(swap! env assoc :on-close :dispose)
+(def wallpaper-width (atom 2880))
+(def wallpaper-height (atom 1800))
 
-(def wallpaper-width (atom 200))
-(def wallpaper-height (atom 200))
-
-(defn paint-canvas [c g]
+(defn paint-g2d [c ^Graphics2D g]
   (let [w2 (/ @wallpaper-width 2)
         h2 (/ @wallpaper-height 2)
         min-rad (min w2 h2)
@@ -29,31 +24,30 @@
       (g/scale scale-factor)
       (g/draw
        (g/rect 0 0 @wallpaper-width @wallpaper-height)
-       (g/style :background (c/color "white")))
+       (g/style :background "white"))
       (g/draw
        (g/circle w2 h2 (* 0.8 min-rad))
-       (g/style :background (c/color "black"))))))
+       (g/style :background "black")))))
 
-(defn paint-wallpaper [w h]
-  (let [bufimg (g/buffered-image w h BufferedImage/TYPE_INT_RGB)
+(defn paint-to-image ^BufferedImage [w h]
+  (let [bufimg ^BufferedImage (g/buffered-image w h BufferedImage/TYPE_INT_RGB)
         g (.getGraphics bufimg)]
-    (paint-canvas (Dimension. w h) g)
+    (paint-g2d (Dimension. w h) g)
     bufimg))
 
-(defn make-wallpaper
-  [width height]
+(defn make-wallpaper [w h]
   (try
-    (ImageIO/write (paint-wallpaper width height)
+    (ImageIO/write (paint-to-image w h)
                    "png" (io/file "./resources/wallpaper.png"))
     (catch java.io.IOException e (println e))))
 
-(defn number-input-text
-  [atom]
-  (let [textfield (s/text :text @atom)]
+(defn number-input-text [atom]
+  (let [textfield (s/text :text @atom
+                          :id :num-input)]
     (b/bind
      textfield
      (b/transform #(try (reset! atom (Long/parseLong %))
-                        (catch NumberFormatException e
+                        (catch NumberFormatException _
                           (println "Not a number" %))
                         (catch Exception e
                           (println "Error:" e))))
@@ -63,29 +57,32 @@
       (b/property textfield :background)))
     textfield))
 
-(defn main-panel
-  []
+(defn main-panel []
   (s/border-panel
-   :center (s/canvas :id :canvas :background (c/color "white") :paint paint-canvas)
-   :south (s/horizontal-panel
-           :items [(s/action :name "Create Wallpaper"
-                             :handler (fn [_] (try (make-wallpaper @wallpaper-width @wallpaper-height)
-                                                   (catch Exception e (.printStackTrace e)))))
-                   (s/label "Width:")
-                   (number-input-text wallpaper-width)
-                   (s/label "Height:")
-                   (number-input-text wallpaper-height)])))
+   :center (s/canvas :id :main-canvas :paint paint-g2d)
+   :south (s/flow-panel
+           :items [(s/horizontal-panel
+                    :items [(s/label "Width:")
+                            (number-input-text wallpaper-width)
+                            (s/label "Height:")
+                            (number-input-text wallpaper-height)])
+                   (s/label :text "Generate Wallpaper?"
+                            :h-text-position :right
+                            :v-text-position :bottom)])))
 
-#_(require 'seesaw.dev)
-
-#_(seesaw.dev/show-events (s/text))
-
-(defn load-gui
-  []
+(defn load-gui []
   (s/invoke-later
-   (-> (s/frame :title "simple circle"
-                :content (main-panel)
-                :minimum-size [640 :by 400]
-                :on-close (:on-close @env))
+   (-> (s/dialog :title "Wallpaper Preview"
+                 :id :main-frame
+                 :type :plain
+                 :option-type :ok-cancel
+                 :success-fn (fn [_] (try (make-wallpaper @wallpaper-width @wallpaper-height)
+                                          (catch Exception e (.printStackTrace e))))
+                 :default-option :ok
+                 :content (main-panel)
+                 :minimum-size [640 :by 400]
+                 :on-close :dispose)
        s/pack!
        s/show!)))
+
+(load-gui)
